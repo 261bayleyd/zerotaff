d2c1961982bbc80a55c85a15f409fd6367ef5e42
261bayleyd <261bayley@gmail.com>
2025-02-25 17:40:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function check(){
     let guess = document.getElementById("guess").value;
     let result = document.getElementById("result");
     if (guess == "Zero2025"){
-window.location.replace("https://https://sites.google.com/view/zero-clothing/staff-portal");
+window.location.replace("https://sites.google.com/view/zero-clothing/staff-portal");
     }
     else{
         result.innerHTML = "Incorrect password";
